e2df70979a3b7ebc9f10396f3471832380e67134
Teerin Leepaiboon <l.teerin@gmail.com>
2024-02-05 23:30:41 +0700

    pos machine

--- pages/machines/pos.vue
+++ pages/machines/pos.vue
@@ -10,38 +10,73 @@
     </div>
         
     <div class="row justify-center q-mt-xs">
-        <q-card  class="bg-deep-orange-2" style="width:100%; max-width:200px">
+        <q-card  class="bg-deep-orange-2" style="width:100%; max-width:280px">
             <q-card-section>
                 <div class="row items-center">
                     <div class="col-4  text-center">
-                        <q-avatar color="red" text-color="white" icon="directions" size="60px"/>
+                        <q-avatar color="red" text-color="white" size="60px">10</q-avatar>
                     </div>
                     <div class="col-8 ">
-                        <span class="text-h6 q-mx-sm">DM-010</span>
+                        <span class="text-h4 q-mx-sm">DM-010</span>
                         <div class="text-h6 q-mx-sm">10KG</div>
                     </div>
                 </div>
             </q-card-section>
-            <div class="row items-center justify-center text-h6">
+            <div class="row items-center justify-center text-h4">
                 Selected: 30
             </div>
-            <div class="row items-center justify-center">
-                <q-btn rounded class="bg-deep-orange-10" style="width:150px">
+            <div class="row items-center justify-center q-my-sm">
+                <q-btn rounded class="bg-deep-orange-7" style="width:240px" >
                     <div class="col-4 text-white">
                         <div class="text-h4">30</div>
                         <div class="text-subtitle2">Price</div>
                     </div>
                     <div class="col-8 text-white">
-                        <div class="text-subtitle1">33 mins</div>
-                        <div class="text-subtitle1">30°</div>
+                        <div class="text-subtitle1">Time: 33 mins</div>
+                        <div class="text-subtitle1">Water: 30°</div>
                     </div>
                 </q-btn>
             </div>
-               
+            <div class="row items-center justify-center q-my-sm">
+                <q-btn rounded class="bg-deep-orange-7" style="width:240px">
+                    <div class="col-4 text-white">
+                        <div class="text-h4">40</div>
+                        <div class="text-subtitle2">Price</div>
+                    </div>
+                    <div class="col-8 text-white">
+                        <div class="text-subtitle1">Time: 60 mins</div>
+                        <div class="text-subtitle1">Water: 40°</div>
+                    </div>
+                </q-btn>
+            </div>
+            <div class="row items-center justify-center q-mt-sm  q-mb-md">
+                <q-btn rounded class="bg-deep-orange-7" style="width:240px">
+                    <div class="col-4 text-white">
+                        <div class="text-h4">50</div>
+                        <div class="text-subtitle2">Price</div>
+                    </div>
+                    <div class="col-8 text-white">
+                        <div class="text-subtitle1">Time: 90 mins</div>
+                        <div class="text-subtitle1">Water: 60°</div>
+                    </div>
+                </q-btn>
+            </div>   
         </q-card>
     </div>
-
-   
+    <div class="row justify-center q-mt-md">
+        <q-btn rounded class="bg-green-7" style="width:260px">
+                <div class="text-h6 text-white">
+                    ADD TO CART
+                </div>
+        </q-btn>
+    </div>
+    <div class="row justify-center q-mt-md">
+        <q-btn rounded class="bg-blue-7" style="width:260px">
+                <div class="text-h6 text-white">
+                    CHECK OUT
+                </div>
+        </q-btn>
+    </div>
 </template>
 
 <script setup lang="ts">
